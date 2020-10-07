58617cf51cc81176ac16455d50477d21fe8cbfee
roniets <rts.21@hotmail.com>
2020-10-07 20:16:05 -0300

    Inserção da últimas funcionalidades
    Finalização das funcionalidades com a inserção dos controles de jogadas e definição do vencedor.

--- jogo_da_velha/js/jogo.js
+++ jogo_da_velha/js/jogo.js
@@ -1,5 +1,21 @@
 var rodada = 1;
-var matriz = Array(3);
+var matriz_jogo = Array(3);
+
+matriz_jogo['a'] = Array(3);
+matriz_jogo['b'] = Array(3);
+matriz_jogo['c'] = Array(3);
+
+matriz_jogo['a'][1] = 0;
+matriz_jogo['a'][2] = 0;
+matriz_jogo['a'][3] = 0;
+
+matriz_jogo['b'][1] = 0;
+matriz_jogo['b'][2] = 0;
+matriz_jogo['b'][3] = 0;
+
+matriz_jogo['c'][1] = 0;
+matriz_jogo['c'][2] = 0;
+matriz_jogo['c'][3] = 0;
 
 $(document).ready(function(){
 
@@ -24,6 +40,7 @@ $(document).ready(function(){
         $('.jogada').click(function(){
             
             var id_campo_clicado = this.id;
+            $('#'+id_campo_clicado).off();
             jogada(id_campo_clicado);
 
         });
@@ -43,6 +60,77 @@ $(document).ready(function(){
             rodada ++;
 
             $('#'+id).css('background-image', icone);
+
+            var linha_coluna = id.split('-');
+
+            matriz_jogo[linha_coluna[0]][linha_coluna[1]] = ponto;
+
+            verifica_combinacao();
+
+        }
+
+        function verifica_combinacao(){
+
+            var pontos = 0;
+
+            for(var i = 1; i <= 3; i++){
+                pontos = pontos + matriz_jogo['a'][i];
+            }
+
+            ganhador(pontos);
+
+            pontos = 0;
+
+            for(var i = 1; i <= 3; i++){
+                pontos = pontos + matriz_jogo['b'][i];
+            }
+
+            ganhador(pontos);
+
+            pontos = 0;
+
+            for(var i = 1; i <= 3; i++){
+                pontos = pontos + matriz_jogo['c'][i];
+            }
+
+            ganhador(pontos);
+
+            for(var l = 1; l <= 3; l++){
+
+                pontos = 0;
+
+                pontos += matriz_jogo['a'][l];
+                pontos += matriz_jogo['b'][l];
+                pontos += matriz_jogo['c'][l];
+
+                ganhador(pontos);
+            }
+
+            pontos = 0;
+
+            pontos = matriz_jogo['a'][1] + matriz_jogo['b'][2] + matriz_jogo['c'][3];
+
+            ganhador(pontos);
+
+            pontos = 0;
+
+            pontos = matriz_jogo['a'][3] + matriz_jogo['b'][2] + matriz_jogo['c'][1];
+
+            ganhador(pontos);
+
+        }
+
+        function ganhador(pontos){
+
+            if(pontos == -3){
+                var jogada_1 = $('#apelido_jogador1').val();
+                alert(jogada_1 + ' é o vencedor!');
+                $('.jogada').off();
+            } else if(pontos == 3){
+                var jogada_2 = $('#apelido_jogador2').val();
+                alert(jogada_2 + ' é o vencedor!');
+                $('.jogada').off();
+            }
 
         }
 
